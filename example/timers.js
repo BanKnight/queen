942a504d00de272a4c65850947cd3d03f4cbfc86
Banknight <tufeigd@163.com>
2022-05-16 21:18:16 +0800

    修复bug

--- example/timers.js
+++ example/timers.js
@@ -5,5 +5,12 @@ module.exports = function (ant)
         console.log("ant entry", ant.id, ant.template, ant.args)
 
         ant.setImmediate(console.log, "aaaaa", "bbbb")
+
+        let id = ant.setInterval(console.log, 3000, "interval 3s", "cccc")
+
+        console.log("setInterval", id)
+
+        ant.setTimeout(ant.clearInterval.bind(ant, id), 5000)
+
     })
 }
