d997b1361cf8a6e88d9490bfc4c877589b394073
Banknight <tufeigd@163.com>
2022-05-27 12:34:20 +0800

    增加comp的mixin功能

--- example/comp.js
+++ example/comp.js
@@ -1,26 +1,37 @@
-let define = null
-
-module.exports = function (ant)
-{
-    ant.on("start", () =>
+const father = {
+    data()
     {
-        ant.add_comp(define, {
-            c: "10",
-            on: {
-                changed()
-                {
-                    this.ant.console.log("changed", this.c)
-                }
+        this.father_count = "father hello"
+    },
+    hooks: {
+        start()
+        {
+
+            this.ant.console.log("father start")
+        },
+        exit()
+        {
+            this.ant.console.log("father stop")
+
+        }
+    },
+    timers: {
+        father_second: {
+            interval: 1000,
+            handler()
+            {
+                this.ant.console.log("this is father second")
             }
-        })
-    })
+        }
+    },
 }
 
-define = {
+const define = {
     data()
     {
         this.count = 0
     },
+    mixins: [father],
     props: {
         a: 1,
         b: {
@@ -43,12 +54,12 @@ define = {
     },
     timers: {
         second: {
-            interval: 5000,
+            interval: 1000,
             handler()
             {
                 this.count++
 
-                this.ant.console.log("this is 5 second", this.a, this.b, this.c, this.count)
+                this.ant.console.log("timer[1s]", this.count, this.a, this.b, this.c, this.father_count)
 
                 this.emit("changed")
 
@@ -59,4 +70,20 @@ define = {
             }
         }
     }
+}
+
+module.exports = function (ant)
+{
+    ant.on("start", () =>
+    {
+        ant.add_comp(define, {
+            c: "10",
+            on: {
+                changed()
+                {
+                    this.ant.console.log("changed", this.c)
+                }
+            }
+        })
+    })
 }
